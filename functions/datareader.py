d913efa984509d4484a4888db95a8fc292caff84
Pasin Sirirat <pasin@Pasins-MacBook-Pro.local>
2021-12-04 12:09:17 +0700

    add fetching transaction volume

--- functions/datareader.py
+++ functions/datareader.py
@@ -2,10 +2,10 @@ import pandas_datareader.data as web
 import pandas as pd 
 import datetime as dt
 
-def pull_stock_data(stocks, start, end, price_type = 'Close', source = 'yahoo'):
+def pull_stock_data(stocks, start, end, columns = ['Close'], source = 'yahoo', **kwargs):
     raw = web.DataReader(stocks, source, start, end)
-    cols = [col for col in raw.columns if col[0] == 'Close']
+    cols = [col for col in raw.columns if col[0] in columns]
 
     df = raw[cols]
-    df.columns = [col[1] for col in df.columns]
+    df.columns = ['_'.join(col) for col in df.columns]
     return df
