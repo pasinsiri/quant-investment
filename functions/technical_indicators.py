141f77a99f51bfae7c889d02b7d83e3498a59153
Pasin Sirirat <pasin@Pasins-MacBook-Pro.local>
2022-08-22 21:53:08 +0700

    bugfix/typo

--- functions/technical_indicators.py
+++ functions/technical_indicators.py
@@ -6,30 +6,29 @@ class TechnicalIndicators():
         pass 
     
     # * moving correlation
-    def corr_over_time(self, s1:pd.Series, s2:pd.Series, n:int = 10, window_mode:str = 'rolling'):
+    def corr_over_time(self, s1:pd.Series, s2:pd.Series, start_n:int = 10, window_mode:str = 'rolling'):
         """calculate correlations over time. note that both series should have datetime as their indices, and it should be the same for each row
 
         Args:
             s1 (pd.Series): the first series
             s2 (pd.Series): the other one
-            n (int, optional): number of backward rows used to calculate correlation. Defaults to 10.
+            start_n (int, optional): number of backward rows used to calculate correlation. Defaults to 10.
         """
 
-        # ! edit later
-        # if s1.index != s2.index:
-        #     raise ValueError('indices of s1 and s2 do not match each other')
+        if s1.index.equals(s2.index):
+            raise ValueError('indices of s1 and s2 do not match each other')
 
-        out = pd.Series(index=s1.index.tolist()[n:])
+        out = pd.Series(index=s1.index.tolist()[start_n:])
         if window_mode == 'expanding':
-            for n in range(n, s1.shape[0]):
+            for n in range(start_n, s1.shape[0]):
                 tmp_s1 = s1.iloc[:n]
                 tmp_s2 = s2.iloc[:n]
-                out.iloc[n - n] = tmp_s1.corr(tmp_s2)
+                out.iloc[n - start_n] = tmp_s1.corr(tmp_s2)
         elif window_mode == 'rolling':
-            for n in range(n, s1.shape[0]):
-                tmp_s1 = s1.iloc[n - n:n]
-                tmp_s2 = s2.iloc[n - n:n]
-                out.iloc[n - n] = tmp_s1.corr(tmp_s2)
+            for n in range(start_n, s1.shape[0]):
+                tmp_s1 = s1.iloc[n - start_n:n]
+                tmp_s2 = s2.iloc[n - start_n:n]
+                out.iloc[n - start_n] = tmp_s1.corr(tmp_s2)
                 pass 
         else:
             raise ValueError('window_mode can be either rolling or expanding')
