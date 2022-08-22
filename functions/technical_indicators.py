4b95ec5e1e46b1f86f8e363a219c513d33577821
Pasin Sirirat <pasin@Pasins-MacBook-Pro.local>
2022-08-22 21:43:02 +0700

    add option to expand/roll moving corr

--- functions/technical_indicators.py
+++ functions/technical_indicators.py
@@ -1,4 +1,3 @@
-from multiprocessing.sharedctypes import Value
 import pandas as pd 
 import numpy as np 
 
@@ -7,28 +6,37 @@ class TechnicalIndicators():
         pass 
     
     # * moving correlation
-    def corr_over_time(s1:pd.Series, s2:pd.Series, start_n:int = 10):
+    def corr_over_time(self, s1:pd.Series, s2:pd.Series, n:int = 10, window_mode:str = 'rolling'):
         """calculate correlations over time. note that both series should have datetime as their indices, and it should be the same for each row
 
         Args:
             s1 (pd.Series): the first series
             s2 (pd.Series): the other one
-            start_n (int, optional): number of backward rows used to calculate correlation. Defaults to 10.
+            n (int, optional): number of backward rows used to calculate correlation. Defaults to 10.
         """
 
-        if s1.index != s2.index:
-            raise ValueError('indices of s1 and s2 do not match each other')
+        # ! edit later
+        # if s1.index != s2.index:
+        #     raise ValueError('indices of s1 and s2 do not match each other')
 
         # Create output series
-        out = pd.Series(index=s1.index.tolist()[start_n:])
+        out = pd.Series(index=s1.index.tolist()[n:])
 
         # we use expand window in this case, note that we can also use moving window
-        for n in range(start_n, s1.shape[0]):
-            tmp_s1 = s1.iloc[:n]
-            tmp_s2 = s2.iloc[:n]
-
-            out.iloc[n - start_n] = tmp_s1.corr(tmp_s2)
-        del tmp_s1, tmp_s2
+        if window_mode == 'expanding':
+            for n in range(n, s1.shape[0]):
+                tmp_s1 = s1.iloc[:n]
+                tmp_s2 = s2.iloc[:n]
+                out.iloc[n - n] = tmp_s1.corr(tmp_s2)
+        elif window_mode == 'rolling':
+            for n in range(n, s1.shape[0]):
+                tmp_s1 = s1.iloc[n - n:n]
+                tmp_s2 = s2.iloc[n - n:n]
+                out.iloc[n - n] = tmp_s1.corr(tmp_s2)
+                pass 
+        else:
+            raise ValueError('window_mode can be either rolling or expanding')
+        
         return out
 
     # * relative strength indicator (RSI)
