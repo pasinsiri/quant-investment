dce7ab0e95d025076d1dc6d0a70ab14f5016b770
pasinsiri <pasin_plp0206@hotmail.com>
2023-04-12 18:30:11 +0700

    add combine_factor_forward_returns

--- functions/alpha_factor_evaluator.py
+++ functions/alpha_factor_evaluator.py
@@ -1,5 +1,21 @@
-import alphalens as al 
+import pandas as pd 
+import numpy as np
+import alphalens as al
 
 class AlphaFactorEvaluator():
-    def __init__(self, return_df) -> None:
-        self.return_df = return_df
+    def __init__(self, factor_return, price) -> None:
+        self.factor_return = factor_return
+        self.factor_names = factor_return.columns
+        self.price = price
+
+    def combine_factor_forward_returns(self, period:int, max_loss:float, verbose:bool = False):
+        factor_data_dict = dict()
+        for factor in self.factor_names:
+            if verbose: print(f'Formatting factor data for {factor}')
+            factor_data_dict[factor] = al.utils.get_clean_factor_and_forward_returns(
+                factor = self.factor_return[factor],
+                prices = self.price,
+                periods = [period],
+                max_loss = max_loss
+            )
+        return factor_data_dict
