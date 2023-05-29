a6a2c21589d83cb22669736d80955f369625a592
pasinsiri <pasin_plp0206@hotmail.com>
2023-05-29 21:36:19 +0700

    add YFinanceReader to stock_reader

---
+++ stock_reader.py
@@ -0,0 +1,19 @@
+import json 
+import datetime as dt 
+from functions.datareader import YFinanceReader
+
+# TODO: set parameters
+start = dt.date(2022, 4, 1)
+end = dt.date(2023, 5, 29)
+ANNUALIZATION_FACTOR = 252
+
+# TODO: load stock and sector data
+with open('./keys/set_sectors.json', 'r') as f:
+    sectors = json.load(f)
+
+all_tickers = sectors.values()
+all_tickers = [v + '.BK' for s in all_tickers for v in s]
+
+yfr = YFinanceReader(stock_sectors = sectors, market_suffix = '.BK')
+yfr.load_data(period = '1m')
+yfr.save('./data/set', start_writing_date=dt.datetime(2022, 4, 1))
