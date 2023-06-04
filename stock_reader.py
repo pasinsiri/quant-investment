0b8221d2d3d4bf7b6e3e1a36e8d121d30e6742d1
pasinsiri <pasin_plp0206@hotmail.com>
2023-06-04 10:49:01 +0700

    add market_suffix

--- stock_reader.py
+++ stock_reader.py
@@ -5,14 +5,15 @@ from functions.datareader import YFinanceReader
 # TODO: set parameters
 start = dt.date(2022, 5, 1)
 ANNUALIZATION_FACTOR = 252
+MARKET_SUFFIX = '.BK'
 
 # TODO: load stock and sector data
 with open('./keys/set_sectors.json', 'r') as f:
     sectors = json.load(f)
 
 all_tickers = sectors.values()
-all_tickers = [v + '.BK' for s in all_tickers for v in s]
+all_tickers = [v + MARKET_SUFFIX for s in all_tickers for v in s]
 
-yfr = YFinanceReader(stock_sectors = sectors, market_suffix = '.BK')
+yfr = YFinanceReader(stock_sectors = sectors, market_suffix = MARKET_SUFFIX)
 yfr.load_data(period = '1y')
 yfr.save('./data/set', start_writing_date=start)
