ab1b437d66219b1327be5b8db739b8d5cb6942a3
pasinsiri <pasin_plp0206@hotmail.com>
2023-06-11 19:40:38 +0700

    add estimate_factor_returns

--- functions/backtest.py
+++ functions/backtest.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import patsy
 import tqdm
+from statsmodels.formula.api import ols
 
 class BackTestPrep():
     def __init__(self, factor_df, covariance, return_df, alpha_factors:list, risk_factors:list, **kwargs) -> None:
@@ -20,3 +21,24 @@ class BackTestPrep():
         data.set_index([data.index, 'Barrid'], inplace=True)
         join_df = data.merge(self.return_df, left_index=True, right_index=True, how='left')
         return join_df
+    
+    def wins(x, lower:float, upper:float):
+        return np.where(x <= lower, lower, np.where(x >= upper, upper, x))
+    
+    def get_formula(factors, Y):
+        L = ["0"]
+        L.extend(factors)
+        return f'{Y} ~ {" + ".join(L)}'
+    
+    def factors_from_names(n):
+        return list(filter(lambda x: "USFASTD_" in x, n))
+
+    def estimate_factor_returns(self, df, return_col:str = 'DlyReturn'):     
+        # * winsorize returns for fitting 
+        df[return_col] = self.wins(df[return_col], -0.25, 0.25)
+    
+        all_factors = self.factors_from_names(list(df))
+        form = self.get_formula(all_factors, return_col)
+        model = ols(form, data=df)
+        results = model.fit()
+        return results
