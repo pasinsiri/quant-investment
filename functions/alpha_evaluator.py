ef563d29cea963c4b35caefe7d3b3ad656ede93b
pasinsiri <pasin_plp0206@hotmail.com>
2023-07-29 21:32:41 +0700

    add get_factor_weights

--- functions/alpha_evaluator.py
+++ functions/alpha_evaluator.py
@@ -38,6 +38,31 @@ class AlphaFactorEvaluator():
                 max_loss = max_loss
             )
         return factor_data_dict
+    
+    def get_factor_weights(self, factor_data_dict:dict, demeaned:bool = False, group_adjust:bool = False, equal_weight:bool = False):
+        """utilize the alphalens library to compute factor-wise asset weights in a specific period. The weights is calculated by normalizing the factor values of a given factor (dividing by the absolute sum of such factor values in a given time). Please note that if all factor values in the period are positive. The weights will range from zero to one and the sum of all weights will be one. Otherwise it may not being so.
+
+        Args:
+            factor_data_dict (dict): a dictionary of a single factor dataframes with factor name as keys and factor value as values (you can obtain this dict from compute_factor_forward_returns)
+            demeaned (bool, optional): should this computation happen on a long short portfolio? if True, weights are computed by demeaning factor values and dividing by the sum of their absolute value (achieving gross leverage of 1). The sum of positive weights will be the same as the negative weights (absolute value), suitable for a dollar neutral long-short portfolio. Defaults to False
+            group_adjust (bool, optional): should this computation happen on a group neutral portfolio? If True, compute group neutral weights: each group will weight the same and if 'demeaned' is enabled the factor values demeaning will occur on the group level. Defaults to False
+            equal_weight (bool, optional): if True the assets will be equal-weighted instead of factor-weighted. If demeaned is True then the factor universe will be split in two equal sized groups, top assets with positive weights and bottom assets with negative weights. Defaults to False
+
+        Returns:
+            pd.DataFrame: a multi-index dataframe of asset weights
+        """
+        factor_weight_list = []
+        for factor in self.factor_names:
+            factor_weight = al.performance.factor_weights(
+                factor_data=factor_data_dict[factor],
+                demeaned=demeaned,
+                group_adjust=group_adjust,
+                equal_weight=equal_weight
+            ).to_frame()
+            factor_weight.columns = [factor]
+            factor_weight_list.append(factor_weight)
+
+        return pd.concat(factor_weight_list, axis=1)
 
     def get_factor_returns(self, factor_data_dict, by_asset:bool = False, demeaned:bool = False, group_adjust:bool = False, equal_weight:bool = False):
         """utilize the alphalens library to calculate factor returns from factor values
