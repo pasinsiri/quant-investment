5f65ce7f8c4b3331a5537596c2778180fc9526b5
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-14 18:09:53 +0700

    initialize auto_alert.py

---
+++ auto_alert.py
@@ -0,0 +1,6 @@
+import pandas_datareader as pdr
+import pandas as pd
+import numpy as np
+from functions.data_reader import YFinanceReader
+
+ticker_list = ['AAPL', 'FB', 'GOOG', 'NFLX', 'NVDA', 'TSLA']
