a64cee865fe6839c26a517dd4f2312747ec933e3
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-22 21:50:23 +0700

    refactor finno_api

--- functions/finno_api.py
+++ functions/finno_api.py
@@ -1,42 +1,51 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import requests
+
 
 class FinnoFund():
     def __init__(self) -> None:
         self.base_url = 'https://api.finnomena.com/fund-service/public/api/v2/'
         self.fund_data = self.get_fund_data()[['fund_id', 'short_code']]
 
-    def get_fund_data(self, filter_sec_active:bool = True):
+    def get_fund_data(self, filter_sec_active: bool = True):
         url = self.base_url + 'funds'
-        res = requests.get(url) 
+        res = requests.get(url)
         if res.status_code != 200:
-            raise requests.RequestException(f'request failed with status {res.status_code}')
-        fund_df = pd.DataFrame(res.json()['data']) 
+            raise requests.RequestException(
+                f'request failed with status {res.status_code}')
+        fund_df = pd.DataFrame(res.json()['data'])
         if filter_sec_active:
-            return fund_df[fund_df['sec_is_active'] == 1].reset_index(drop = True) 
+            return fund_df[fund_df['sec_is_active']
+                           == 1].reset_index(drop=True)
         else:
-            return fund_df 
+            return fund_df
 
-    def get_fund_price(self, fund_name:str):
-        fund_id = self.fund_data[self.fund_data['short_code'] == fund_name]['fund_id'].values[0]
+    def get_fund_price(self, fund_name: str):
+        fund_id = self.fund_data[self.fund_data['short_code']
+                                 == fund_name]['fund_id'].values[0]
         url = self.base_url + f'funds/{fund_id}/nav/q?range=MAX'
-        res = requests.get(url) 
+        res = requests.get(url)
         if res.status_code != 200:
-            raise requests.RequestException(f'request failed with status {res.status_code}')
+            raise requests.RequestException(
+                f'request failed with status {res.status_code}')
         fund_df = pd.DataFrame(res.json()['data']['navs'])
         fund_df['date'] = pd.to_datetime(fund_df['date'])
         fund_df['date'] = [d.date() for d in fund_df['date']]
         return fund_df.set_index('date')
-    
-    def get_multiple_fund_price(self, fund_list:list):
+
+    def get_multiple_fund_price(self, fund_list: list):
         all_df = None
         for fund in fund_list:
             fund_df = self.get_fund_data(fund)
             fund_df.columns = ['_'.join([fund, c]) for c in fund_df.columns]
 
             if all_df is None:
-                all_df = fund_df 
+                all_df = fund_df
             else:
-                all_df = all_df.merge(fund_df, left_index=True, right_index=True, how='outer').sort_index()
-        return all_df 
+                all_df = all_df.merge(
+                    fund_df,
+                    left_index=True,
+                    right_index=True,
+                    how='outer').sort_index()
+        return all_df
