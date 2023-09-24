6142899d84bf2772a84c3be5ee9d41e71d8e420b
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-24 20:32:11 +0700

    add display index name

--- auto_alert.py
+++ auto_alert.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yfinance as yf
+import json
 
 def flag_new_high_new_low(series, n:int = 20):
     series = series.iloc[series.shape[0] - n:]
@@ -23,19 +24,24 @@ def flag_ma(series, n:int = 20):
     else:
         return None
 
-if __name__ == 'main':
-    ticker_list = ['AAPL', 'GOOG', 'NFLX', 'NVDA', 'TSLA']
-    yfinance_meta = yf.Tickers(ticker_list)
-    raw_df = yfinance_meta.history(period='1y', auto_adjust=True, progress=False)
-    raw_df.index = pd.to_datetime(raw_df.index)
+# TODO: load ticker list
+with open('./meta/auto_alert_tickers.json', 'r') as f:
+    tickers = json.load(f)
+# print(tickers['Indexes'].keys())
 
-    close_cols = [c for c in raw_df.columns if c[0] == 'Close']
-    close_df = raw_df[close_cols]
-    close_df.columns = [c[1] for c in close_df.columns]
-    close_df.head()
+yfinance_meta = yf.Tickers(list(tickers['Indexes'].keys()))
+raw_df = yfinance_meta.history(period='1y', auto_adjust=True, progress=False)
+# print(raw_df.columns)
+raw_df.columns = [(c[0], tickers['Indexes'][c[1]]) for c in raw_df.columns]
+raw_df.index = pd.to_datetime(raw_df.index)
 
-    res = close_df.apply(flag_new_high_new_low, axis=0, n=7)
-    res = res[res.notnull()]
-    msg_list = [': '.join([i, res.loc[i]]) for i in res.index]
-    for msg in msg_list:
-        print(msg)
+close_cols = [c for c in raw_df.columns if c[0] == 'Close']
+close_df = raw_df[close_cols]
+close_df.columns = [c[1] for c in close_df.columns]
+close_df.head()
+
+res = close_df.apply(flag_new_high_new_low, axis=0, n=7)
+res = res[res.notnull()]
+msg_list = [': '.join([i, res.loc[i]]) for i in res.index]
+for msg in msg_list:
+    print(msg)
