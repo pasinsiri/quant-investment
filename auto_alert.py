2e0d022efab0da270011ad8c511269bf1c1c9686
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-04 21:42:17 +0700

    add technical indicators to auto alert

--- auto_alert.py
+++ auto_alert.py
@@ -25,11 +25,6 @@ def flag_ma(series, n:int = 20):
     else:
         return None
 
-# TODO: technical indicators
-# ti = TechnicalIndicators()
-
-
-
 # TODO: load ticker list
 with open('./keys/global_indexes.json', 'r') as f:
     tickers = json.load(f)
@@ -41,12 +36,25 @@ raw_df.columns = [(c[0], tickers['Indexes'][c[1]]) for c in raw_df.columns]
 raw_df.index = pd.to_datetime(raw_df.index)
 
 close_cols = [c for c in raw_df.columns if c[0] == 'Close']
-close_df = raw_df[close_cols]
-close_df.columns = [c[1] for c in close_df.columns]
-close_df.head()
+# close_df = raw_df[close_cols]
+# close_df.columns = [c[1] for c in close_df.columns]
+# close_df.head()
 
-res = close_df.apply(flag_new_high_new_low, axis=0, n=7)
-res = res[res.notnull()]
-msg_list = [': '.join([i, res.loc[i]]) for i in res.index]
-for msg in msg_list:
-    print(msg)
+# TODO: technical indicators
+for ticker in ticker_list:
+    ticker_df = raw_df[raw_df[ticker]]
+    ticker_df.columns = [c[1] for c in ticker_df.columns]
+    ti = TechnicalIndicators(ticker_df)
+
+    res_dict = dict()
+
+    # * add indicators
+    res_dict['rsi'] = ti.RSI(n=14)
+    res_dict['macd'], res_dict['macd_signal'] = ti.MACD(n_long=26, n_short=12)
+    res_dict['bollinger_ratio'] = ti.bollinger_ratio(n=20, k=2)
+
+# res = close_df.apply(flag_new_high_new_low, axis=0, n=7)
+# res = res[res.notnull()]
+# msg_list = [': '.join([i, res.loc[i]]) for i in res.index]
+# for msg in msg_list:
+#     print(msg)
