763c24a9293f4da76ae628754d8ec652b5aeaea7
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-29 21:42:49 +0700

    create export dict

--- set_scraper.py
+++ set_scraper.py
@@ -1,4 +1,5 @@
 import json
+import datetime as dt
 from functions.scraper import SETScraper
 
 INDEXES_URL = {
@@ -7,9 +8,15 @@ INDEXES_URL = {
     'sset': 'https://www.set.or.th/th/market/index/sset/overview',
     'mai': 'https://www.set.or.th/th/market/index/mai/overview',
 }
+EXPORT_PATH = './test/index_list/set'
 
 tickers = {}
 scraper = SETScraper(driver_type='firefox')
 for index, url in INDEXES_URL.items():
     res = scraper.get_ticker_list(url=url, tag_name='div', tag_attrs={'class': 'btn-quote'})
     tickers[index] = res
+
+# * create an export dictionary
+export = {}
+export['data_date'] = dt.date.today()
+export['data'] = tickers
