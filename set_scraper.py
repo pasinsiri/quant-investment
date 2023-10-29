58a5a0541490979785c0fe303ebcdee9c2466366
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-29 20:54:18 +0700

    add url list

---
+++ set_scraper.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+from functions.scraper import SETScraper
+
+URL_LIST = [
+    'https://www.set.or.th/th/market/index/set50/overview',
+    'https://www.set.or.th/th/market/index/set100/overview',
+    'https://www.set.or.th/th/market/index/sset/overview'
+]
