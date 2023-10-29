31254f5284766fb7bd14fac0c7d9d8f737c227a6
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-29 20:54:48 +0700

    add mai url

--- set_scraper.py
+++ set_scraper.py
@@ -5,5 +5,6 @@ from functions.scraper import SETScraper
 URL_LIST = [
     'https://www.set.or.th/th/market/index/set50/overview',
     'https://www.set.or.th/th/market/index/set100/overview',
-    'https://www.set.or.th/th/market/index/sset/overview'
+    'https://www.set.or.th/th/market/index/sset/overview',
+    'https://www.set.or.th/th/market/index/mai/overview'
 ]
