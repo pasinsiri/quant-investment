759ecd34aeae881576d48d3bf0342fcc07045fdd
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-29 21:43:46 +0700

    add logging message

--- set_scraper.py
+++ set_scraper.py
@@ -1,5 +1,7 @@
 import json
 import datetime as dt
+import logging
+import time
 from functions.scraper import SETScraper
 
 INDEXES_URL = {
@@ -15,8 +17,14 @@ scraper = SETScraper(driver_type='firefox')
 for index, url in INDEXES_URL.items():
     res = scraper.get_ticker_list(url=url, tag_name='div', tag_attrs={'class': 'btn-quote'})
     tickers[index] = res
+    logging.info(f'Scraping {index} completed')
+    time.sleep(5)
 
 # * create an export dictionary
 export = {}
 export['data_date'] = dt.date.today()
 export['data'] = tickers
+
+# * save data
+with open(EXPORT_PATH, 'w') as f:
+    json.dump(export, f)
