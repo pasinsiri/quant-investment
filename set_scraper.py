c8ec7d818cdf4811822d343b100c54669e0cbc78
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-29 21:40:51 +0700

    scrape ticker list for thai indexes

--- set_scraper.py
+++ set_scraper.py
@@ -1,14 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
+import json
 from functions.scraper import SETScraper
 
-URL_LIST = [
-    'https://www.set.or.th/th/market/index/set50/overview',
-    'https://www.set.or.th/th/market/index/set100/overview',
-    'https://www.set.or.th/th/market/index/sset/overview',
-    'https://www.set.or.th/th/market/index/mai/overview'
-]
+INDEXES_URL = {
+    'set50': 'https://www.set.or.th/th/market/index/set50/overview',
+    'set100': 'https://www.set.or.th/th/market/index/set100/overview',
+    'sset': 'https://www.set.or.th/th/market/index/sset/overview',
+    'mai': 'https://www.set.or.th/th/market/index/mai/overview',
+}
 
+tickers = {}
 scraper = SETScraper(driver_type='firefox')
-for url in URL_LIST:
-    pass
+for index, url in INDEXES_URL.items():
+    res = scraper.get_ticker_list(url=url, tag_name='div', tag_attrs={'class': 'btn-quote'})
+    tickers[index] = res
