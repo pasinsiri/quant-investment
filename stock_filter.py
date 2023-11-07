d0f5d2a2a6042dfa9e1150e3d0acf74c50492326
pasinsiri <pasin_plp0206@hotmail.com>
2023-11-07 23:11:10 +0700

    add while loops

--- stock_filter.py
+++ stock_filter.py
@@ -5,3 +5,18 @@ from dateutil.relativedelta import relativedelta
 target_date = dt.date.today()
 start_date = (target_date - relativedelta(years=1)).replace(day=1)
 base_path = './data/prices/set/'
+
+paths = {}
+res = {}
+while True:
+    y, m = '{:04d}'.format(start_date.year), '{:02d}'.format(start_date.month)
+    ym_path = os.path.join(base_path, y, m)
+    paths[f'{y}{m}'] = ym_path
+
+    start_date += relativedelta(months=1)
+    if start_date > target_date:
+        break
+
+res = {key: pd.read_parquet(path) for key, path in paths.items()}
+raw_df = pd.concat(res.values(), axis=0)
+raw_df.head()
