48f6c679647951a4cb4aa4209575934715e24fae
pasinsiri <pasin_plp0206@hotmail.com>
2023-11-07 23:10:56 +0700

    add paths

--- stock_filter.py
+++ stock_filter.py
@@ -1,3 +1,7 @@
 import numpy as np
 import pandas as pd
 from dateutil.relativedelta import relativedelta
+
+target_date = dt.date.today()
+start_date = (target_date - relativedelta(years=1)).replace(day=1)
+base_path = './data/prices/set/'
