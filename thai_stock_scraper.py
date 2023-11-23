b30056a796f8b8831a0b11e19214a970846d3ec4
pasinsiri <pasin_plp0206@hotmail.com>
2023-11-23 20:43:22 +0700

    add save data

--- thai_stock_scraper.py
+++ thai_stock_scraper.py
@@ -1,5 +1,8 @@
+import os
 from functions.scraper import SiamchartScraper
 
-export_path = ''
+export_path = os.path.join('content/thai/ticker_list', 'thai_ticker.csv')
 ss = SiamchartScraper()
 info = ss.get_ticker_info_table()
+info.to_csv(export_path, index=False)
+print('Scraping completed')
