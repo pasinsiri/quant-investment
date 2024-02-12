c5828c86931daf8d14975bd6dcb4a125c7ccf352
pasinsiri <pasin_plp0206@hotmail.com>
2024-02-12 20:58:55 +0700

    export ticker to json

--- thai_stock_scraper.py
+++ thai_stock_scraper.py
@@ -6,5 +6,6 @@ export_path = os.path.join('content/thai/ticker_list', 'thai_ticker.json')
 ss = SiamchartScraper()
 info = ss.get_ticker_info_table(return_type='json')
 # info.to_csv(export_path, index=False)
-with open('')
+with open(export_path, 'w') as f:
+    json.dump(info, f)
 print('Scraping completed')
