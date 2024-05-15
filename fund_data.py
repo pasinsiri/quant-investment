fa88ebbf99351d692a3a79f7cbd9724eca97335f
pasinsiri <pasin_plp0206@hotmail.com>
2024-05-15 11:10:41 +0700

    create iteration

--- fund_data.py
+++ fund_data.py
@@ -1,3 +1,12 @@
 import numpy as np
 import pandas as pd
 from functions.finno_api import FinnoFund
+
+ff = FinnoFund()
+fund_data = ff.get_fund_data(filter_sec_active=True)
+
+success_code = []
+
+# * iterate
+c = 0
+nav_df = pd.DataFrame()
