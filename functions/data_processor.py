2d60285df75a5ed2d2e6e5aed2c0ad56202c62b9
pasinsiri <pasin_plp0206@hotmail.com>
2024-12-10 20:50:28 +0700

    add custom_load

--- functions/data_processor.py
+++ functions/data_processor.py
@@ -2,8 +2,16 @@ import pandas as pd
 import numpy as np
 import os
 
-def custom_load(base_path: str):
-    pass
+def custom_load(base_path: str, ticker: str, first_year: int, last_year: int):
+    paths = []
+    for year in range(first_year, last_year + 1):
+        for month in range(1, 13):
+            m_str = '{:02d}'.format(month)
+            path = f'{base_path}/{year}/{m_str}/{ticker}.parquet'
+            if os.path.exists(path):
+                paths.append(path)
+            else:
+                # print(f'{path} not found')
+                pass
+    return paths
 
-def adjust_price(df):
-    pass
