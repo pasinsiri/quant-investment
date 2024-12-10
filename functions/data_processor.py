54799ef2d7d2e629f7eaab73695c2c19dfcf7000
pasinsiri <pasin_plp0206@hotmail.com>
2024-12-10 20:56:36 +0700

    shift cum_adj_factor

--- functions/data_processor.py
+++ functions/data_processor.py
@@ -15,10 +15,14 @@ def custom_load(base_path: str, ticker: str, first_year: int, last_year: int):
                 pass
     return paths
 
-def adjust_price(df, ticker_list: list, base_path: str, first_year: int, last_year: int, split_col_name:str = 'stock split'):
+def adjust_price(
+        df, ticker_list: list, base_path: str, first_year: int, last_year: int, 
+        adjust_cols: list = ['Open', 'High', 'Low', 'Close'], split_col_name:str = 'stock split'
+):
     for ticker in ticker_list:
         paths = custom_load(base_path, ticker, first_year, last_year)
         ticker_df = pd.read_parquet(*[paths]).sort_index(ascending=False)
         ticker_df['adjust_factor'] = ticker_df[split_col_name] \
                                         .apply(lambda x: 1 if x == 0 else x)
-        ticker_df['cum_adj_factor'] = ticker_df['adjust_factor'].cumprod()
+        ticker_df['cum_adj_factor'] = ticker_df['adjust_factor'].cumprod() \
+                                        .shift(1).fillna(1)
