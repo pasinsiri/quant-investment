fd89f14f81bee77295458d88ee1926b8f5a72814
pasinsiri <pasin_plp0206@hotmail.com>
2024-12-11 20:58:28 +0700

    add case if no ticker is parsed

--- functions/data_processor.py
+++ functions/data_processor.py
@@ -7,12 +7,15 @@ def get_parquet_paths(base_path: str, first_year: int, last_year: int, ticker: s
     for year in range(first_year, last_year + 1):
         for month in range(1, 13):
             m_str = '{:02d}'.format(month)
-            path = f'{base_path}/{year}/{m_str}/{ticker}.parquet'
-            if os.path.exists(path):
-                paths.append(path)
-            else:
-                # print(f'{path} not found')
+            if ticker is None:
                 pass
+            else:
+                path = f'{base_path}/{year}/{m_str}/{ticker}.parquet'
+                if os.path.exists(path):
+                    paths.append(path)
+                else:
+                    # print(f'{path} not found')
+                    pass
     return paths
 
 def adjust_price(
