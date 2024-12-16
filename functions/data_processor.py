95ebbe0b3daac1e6261e9d82f25fa9f9e8397577
pasinsiri <pasin_plp0206@hotmail.com>
2024-12-16 21:59:38 +0700

    rename function

--- functions/data_processor.py
+++ functions/data_processor.py
@@ -76,7 +76,7 @@ def convert_price_to_raw(
                 os.mkdir(month_path)
             month_df.to_parquet(f'{month_path}/{ticker}.parquet')
 
-def adjust_price(
+def adjust_price_multiple(
         ticker_list: list, base_path: str, export_base_path: str,
         first_year: int, last_year: int,
         adjust_cols: list = ['open', 'high', 'low', 'close', 'dividends'], split_col_name:str = 'stock splits'
