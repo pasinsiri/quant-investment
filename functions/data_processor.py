2a67666dd84002ec3721ed6c6ba6ca5af8826306
pasinsiri <pasin_plp0206@hotmail.com>
2024-12-19 21:06:41 +0700

    adjust prices based on cum_adj_factor

--- functions/data_processor.py
+++ functions/data_processor.py
@@ -107,6 +107,9 @@ def adjust_price(
                                     .apply(lambda x: 1 if x == 0 else x)
     ticker_df['cum_adj_factor'] = ticker_df['adjust_factor'].cumprod() \
                                     .shift(1).fillna(1)
+    for col in adjust_cols:
+        ticker_df[col] = ticker_df[col] * ticker_df['cum_adj_factor']
+
     ticker_df['fwd_div'] = ticker_df['dividends'].shift(1).fillna(0)
     ticker_df['retention_rate'] = 1 - (ticker_df['fwd_div'] / ticker_df['close'])
     ticker_df['accum_retention'] = ticker_df['retention_rate'].cumprod()
